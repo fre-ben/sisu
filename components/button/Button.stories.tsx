6805bcf8b546bd15d8f739fbc7a3f7300f103543
fre-ben <fschocke@gmail.com>
2021-03-16 15:18:40 +0100

    🚧 WIP StartGame Button

--- components/button/Button.stories.tsx
+++ components/button/Button.stories.tsx
@@ -1,21 +1,8 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
-import Button, { ButtonProps } from "./Button";
+import { Meta } from "@storybook/react/types-6-0";
+import StartGameBtn from "./StartGameBtn";
 
 export default {
   title: "Common/Button",
-  component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
+export const startgame = () => StartGameBtn();
