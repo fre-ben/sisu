d4a201d0eaac44985cfd8e3dade764ae75c1b9f1
fre-ben <fschocke@gmail.com>
2021-03-16 20:05:19 +0100

    ✨ Add Start Game button

--- components/button/StartGameBtn.tsx
+++ components/button/StartGameBtn.tsx
@@ -2,7 +2,9 @@ import "./Button.module.css";
 import styles from "./Button.module.css";
 
 function StartGameBtn() {
-  return <button className={styles.menuBtn}>Start Game</button>;
+  return (
+    <button className={`${styles.btn} ${styles.menuBtn}`}>Start Game</button>
+  );
 }
 
 export default StartGameBtn;
