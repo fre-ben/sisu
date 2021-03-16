99b8b27fca2b6d982e18ad30c98d5883a7662d4a
fre-ben <fschocke@gmail.com>
2021-03-16 12:45:18 +0100

    ✨ Add Logo Small component

--- components/logo/Logo.stories.tsx
+++ components/logo/Logo.stories.tsx
@@ -1,8 +1,10 @@
 import { Meta } from "@storybook/react/types-6-0";
 import LogoMain from "./Logo_main";
+import LogoSmall from "./Logo_small";
 
 export default {
   title: "Common/Logo",
 } as Meta;
 
 export const main = () => LogoMain();
+export const small = () => LogoSmall();
---
+++ components/logo/Logo_small.tsx
@@ -0,0 +1,8 @@
+import "./Logo.module.css";
+import styles from "./Logo.module.css";
+
+function LogoSmall() {
+  return <div className={styles.small}>Sisu</div>;
+}
+
+export default LogoSmall;
