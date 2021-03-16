7734c3b2953c7f8fce05116b17e4e721cb272e0a
fre-ben <fschocke@gmail.com>
2021-03-16 20:08:46 +0100

    ♻️ Refactor button css

--- components/button/StartGameBtn.tsx
+++ components/button/StartGameBtn.tsx
@@ -2,9 +2,7 @@ import "./Button.module.css";
 import styles from "./Button.module.css";
 
 function StartGameBtn() {
-  return (
-    <button className={`${styles.btn} ${styles.menuBtn}`}>Start Game</button>
-  );
+  return <button className={styles.btn}>Start Game</button>;
 }
 
 export default StartGameBtn;
