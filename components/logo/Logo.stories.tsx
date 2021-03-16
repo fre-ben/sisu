bf22c76d9cf3d1ad8fcda64c7b8ef41668230574
fre-ben <fschocke@gmail.com>
2021-03-16 12:16:08 +0100

    ✨ Add Logo Main component

---
+++ components/logo/Logo.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta } from "@storybook/react/types-6-0";
+import LogoMain from "./Logo_main";
+
+export default {
+  title: "Common/Logo",
+} as Meta;
+
+export const main = () => LogoMain();
---
+++ components/logo/Logo_main.tsx
@@ -0,0 +1,8 @@
+import "./Logo.module.css";
+import styles from "./Logo.module.css";
+
+function LogoMain() {
+  return <div className={styles.main}>Sisu</div>;
+}
+
+export default LogoMain;
