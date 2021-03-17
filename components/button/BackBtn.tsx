c393518893e7ac9825af886b38541970214a4cf0
fre-ben <fschocke@gmail.com>
2021-03-17 14:57:13 +0100

    ♻️ Remove unnecessary div element

--- components/button/BackBtn.tsx
+++ components/button/BackBtn.tsx
@@ -3,11 +3,11 @@ import styles from "./Button.module.css";
 
 function BackBtn() {
   return (
-    <button className={`${styles.btn} ${styles.backBtn}`}>
-      <div className={styles.backContainer}>
-        <img src="/arrow.svg" alt="arrow" />
-        Back
-      </div>
+    <button
+      className={`${styles.btn} ${styles.backBtn} ${styles.backContainer}`}
+    >
+      <img src="/arrow.svg" alt="arrow" />
+      Back
     </button>
   );
 }
