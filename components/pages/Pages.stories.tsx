2e52431da463986ae57a0b4160a5430829edd971
fre-ben <fschocke@gmail.com>
2021-03-17 11:44:24 +0100

    ♻️ 🔥 Refactor project structure

--- components/pages/Pages.stories.tsx
+++ components/pages/Pages.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta } from "@storybook/react/types-6-0";
-import IndexPage from "./Index";
+import Home from "../../pages";
 
 export default {
   title: "Pages/Main Menu",
   parameters: { layout: "fullscreen" },
 } as Meta;
 
-export const index = () => <IndexPage />;
+export const index = () => <Home />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
-import IndexPage from "../components/pages/Index";
+import RulesBtn from "../components/button/RulesBtn";
+import StartGameBtn from "../components/button/StartGameBtn";
+import Logo from "../components/logo/Logo";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Sisu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <IndexPage />
+        <div className={styles.container}>
+          <Logo size="big" />
+          <div className={styles.menuItems}>
+            <StartGameBtn />
+            <RulesBtn />
+          </div>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
