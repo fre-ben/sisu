eb0faf462964b3a994ce906bfee13bbd41370d1d
fre-ben <fschocke@gmail.com>
2021-03-18 13:58:41 +0100

    ✨ Add Link to Rules button, linking to Rules page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
+import Link from "next/link";
 import RulesBtn from "../components/button/RulesBtn";
 import StartGameBtn from "../components/button/StartGameBtn";
 import Logo from "../components/logo/Logo";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const router = useRouter();
-
-  const goToTest = (e) => {
-    e.preventDefault();
-    router.push("/test");
-  };
-
   return (
     <>
       <Head>
@@ -24,8 +17,12 @@ export default function Home() {
         <div className={styles.container}>
           <Logo size="big" />
           <div className={styles.menuItems}>
-            <StartGameBtn onClick={goToTest} />
-            <RulesBtn onClick={goToTest} />
+            <StartGameBtn onClick={() => console.log("Clicked")} />
+            <Link href="/rules">
+              <a>
+                <RulesBtn onClick={() => console.log("Clicked")} />
+              </a>
+            </Link>
           </div>
         </div>
       </main>
