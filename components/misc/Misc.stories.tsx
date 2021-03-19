2a71b0f035c0d8c4d4c3a59d2cdfb3aaf383e6a9
fre-ben <fschocke@gmail.com>
2021-03-19 23:29:10 +0100

    📖 Add to storybook

--- components/misc/Misc.stories.tsx
+++ components/misc/Misc.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
+import LobbyListItem, { LobbyListItemProps } from "./LobbyListItem";
 import PlayerCount, { PlayerCountProps } from "./PlayerCount";
 
 export default {
@@ -6,9 +7,20 @@ export default {
 } as Meta;
 
 const Template: Story<PlayerCountProps> = (args) => <PlayerCount {...args} />;
+const TemplateListItem: Story<LobbyListItemProps> = (args) => (
+  <LobbyListItem {...args} />
+);
 
 export const Playercounter = Template.bind({});
 
 Playercounter.args = {
   playerCount: 0,
+};
+
+export const Lobbylistitem = TemplateListItem.bind({});
+
+Lobbylistitem.args = {
+  playerCount: 4,
+  lobbyNr: 1,
+  onClick: null,
 };
