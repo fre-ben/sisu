ed16b3bc2e28d4c5d93c213e60a264e206025026
fre-ben <fschocke@gmail.com>
2021-03-22 17:49:34 +0100

    ✨ Add DiscardPile and DrawPile gameboard components

---
+++ components/gameboard/DrawPile.tsx
@@ -0,0 +1,19 @@
+import styles from "./Piles.module.css";
+import { PileProps } from "./types";
+
+function DrawPile({ onClick }: PileProps) {
+  return (
+    <>
+      <section className={styles.container}>
+        <h2 className={styles.drawHeadline}>Draw Pile</h2>
+        <img
+          src="/cards/back.png"
+          className={styles.card}
+          onClick={() => onClick}
+        />
+      </section>
+    </>
+  );
+}
+
+export default DrawPile;
