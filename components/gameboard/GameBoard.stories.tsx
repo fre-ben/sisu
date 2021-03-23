bacf51088c135c1923847669820bb178ea5857e5
Frederik <77615388+fre-ben@users.noreply.github.com>
2021-03-23 14:37:05 +0100

    ✨ Add gameboard components (#32)
    * 🍱 Add arrow_right svg
    
    * ✨ Add types for new components
    
    * ✨ Add Statusbar component
    
    * ✨ Add Roundcounter component
    
    * 💄 Style new components
    
    * 📖 Add new components to storybook
    
    * ✨ Add Restart Game button

--- components/gameboard/GameBoard.stories.tsx
+++ components/gameboard/GameBoard.stories.tsx
@@ -1,13 +1,36 @@
-import { Meta } from "@storybook/react/types-6-0";
+import { Meta, Story } from "@storybook/react/types-6-0";
 import CardGrid from "./CardGrid";
 import DiscardPile from "./DiscardPile";
 import DrawPile from "./DrawPile";
 import OpponentCardGrid from "./OpponentCardGrid";
+import RoundCounter from "./RoundCounter";
+import Statusbar from "./Statusbar";
 import TotalScore from "./TotalScore";
+import { RoundCounterProps, StatusbarProps } from "./types";
 
 export default {
   title: "Common/Gameboard",
 } as Meta;
+
+const TemplateRoundCounter: Story<RoundCounterProps> = (args) => (
+  <RoundCounter {...args} />
+);
+
+export const Roundcounter = TemplateRoundCounter.bind({});
+
+Roundcounter.args = {
+  roundNr: 1,
+};
+
+const TemplateStatusbar: Story<StatusbarProps> = (args) => (
+  <Statusbar {...args} />
+);
+
+export const StatusBar = TemplateStatusbar.bind({});
+
+StatusBar.args = {
+  statusMessage: "Start game if all players are connected.",
+};
 
 export const drawpile = () => <DrawPile onClick={() => alert("test")} />;
 export const discardpile = () => <DiscardPile onClick={() => alert("test")} />;
