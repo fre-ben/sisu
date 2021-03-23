34a6273872609d6c2685802e2c97ad81331637d0
fre-ben <fschocke@gmail.com>
2021-03-23 12:42:34 +0100

    ✨ Add types for new components

--- components/gameboard/types.ts
+++ components/gameboard/types.ts
@@ -3,3 +3,11 @@ import { MouseEventHandler } from "react";
 export type PileProps = {
   onClick: MouseEventHandler<HTMLButtonElement>;
 };
+
+export type RoundCounterProps = {
+  roundNr: number;
+};
+
+export type StatusbarProps = {
+  statusMessage: string;
+};
