b379734af86230ebbfb8f4996f3733187899ee12
fre-ben <fschocke@gmail.com>
2021-03-25 12:24:28 +0100

    ✨ Add socket.io in backend Basic functions and first functions for creating games

--- server/socket.ts
+++ server/socket.ts
@@ -1,13 +1,32 @@
-import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import { createGame, getGames } from "./lib/games";
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  // ...
-});
+let io;
 
-io.on("connection", (socket: Socket) => {
-  console.log(socket + "connected");
-});
+function broadcastListGamesUpdate() {
+  io.emit("list games", getGames());
+}
 
-httpServer.listen(3000);
+export function listenSocket(server) {
+  io = new Server(server, {});
+
+  io.on("connection", (socket: Socket) => {
+    console.log(socket.id + " connected");
+
+    socket.on("disconnect", () => {
+      console.log(socket.id + " disconnected");
+    });
+
+    socket.on("list games", () => {
+      socket.emit("list games", getGames());
+    });
+
+    socket.on("game created", (lobbyNr) => {
+      socket.join(`lobby${lobbyNr}`);
+      console.log("Lobby nr " + lobbyNr + " was created");
+      createGame(lobbyNr);
+      broadcastListGamesUpdate();
+      socket.emit("hand down lobbynr", lobbyNr);
+    });
+  });
+}
