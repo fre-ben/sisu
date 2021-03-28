33e0cede57abfc837fc94d6de88492e37fddb656
fre-ben <fschocke@gmail.com>
2021-03-28 15:55:33 +0200

    ✨ Add getLobbyNr() function Accesses the url via useRouter Hook

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,7 +1,14 @@
+import { useRouter } from "next/router";
+
 export function getPlayerName() {
   return localStorage.getItem("playerName");
 }
 
 export function getSocketID() {
   return localStorage.getItem("socketID");
+}
+
+export function getLobbyNr(): number {
+  const router = useRouter();
+  return +router.query.lobby;
 }
