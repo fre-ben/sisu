a2bc6f58892074ce14d40d58a6fda6df0510bbf5
fre-ben <fschocke@gmail.com>
2021-03-30 12:33:09 +0200

    ♻️ Refactor TotalScore render functions

--- server/lib/games.ts
+++ server/lib/games.ts
@@ -107,10 +107,8 @@ export async function getPlayer(socketID) {
   return player;
 }
 
-export async function getTotalScores(socketID) {
-  const currentGame = await getGame(socketID);
-
-  return currentGame.players.map((player) => {
+export function getTotalScores(lobbyNr) {
+  return games[lobbyNr].players.map((player) => {
     return { name: player.name, score: player.totalScore };
   });
 }
