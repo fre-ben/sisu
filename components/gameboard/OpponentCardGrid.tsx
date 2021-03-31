5a8c6f6adcd285da8c766432039340e0464cf2a0
fre-ben <fschocke@gmail.com>
2021-03-31 13:41:44 +0200

    🎨 Update OpponenCardGrid component to receive props

--- components/gameboard/OpponentCardGrid.tsx
+++ components/gameboard/OpponentCardGrid.tsx
@@ -1,47 +1,34 @@
+import { MouseEventHandler } from "react";
+import { Card } from "../../server/lib/gameTypes";
 import styles from "./CardGrid.module.css";
 
-function OpponentCardGrid(onClick) {
-  // Example data, will be removed later by Props handed down to CardGrid
-  const players = [
-    { name: "Frederik", score: 20 },
-    { name: "Kalle", score: 10 },
-    { name: "Eva", score: 30 },
-    { name: "Louis", score: 60 },
-    { name: "Frederik", score: 20 },
-    { name: "Kalle", score: 10 },
-    { name: "Eva", score: 30 },
-    { name: "Peter", score: 30 },
-  ];
-  // Example data, will be removed later by Props handed down to CardGrid
-  const cards = [
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 8, hidden: true, src: "/cards/08.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 6, hidden: false, src: "/cards/06.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 7, hidden: true, src: "/cards/07.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-    { value: 12, hidden: false, src: "/cards/12.png" },
-    { value: 1, hidden: true, src: "/cards/01.png" },
-  ];
+export type OpponentCardGridProps = {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  cards: Card[];
+  name: string;
+  roundScore: number[];
+};
 
+function OpponentCardGrid({
+  // onClick,
+  cards,
+  name,
+  roundScore,
+}: OpponentCardGridProps) {
   const createPlayerCardGrid = cards.map((card) => (
     <img
       key={card.value}
-      src={card.hidden ? "/cards/back.png" : card.src}
-      onClick={onClick}
+      src={card.hidden ? "/cards/back.png" : card.imgSrc}
+      // onClick={onClick}
     />
   ));
 
   return (
     <section className={styles.opponentContainer}>
       <p>
-        Score: <span>{players[0].score}</span>
+        Score: <span>{roundScore}</span>
       </p>
-      <p>{players[0].name}</p>
+      <p>{name}</p>
       <div className={styles.oppCardGrid}>{createPlayerCardGrid}</div>
     </section>
   );
