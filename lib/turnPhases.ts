b8ec2d799d246eb1c203665ab67646e964dfadfe
fre-ben <fschocke@gmail.com>
2021-04-07 16:32:40 +0200

    ✨ Add phases object

---
+++ lib/turnPhases.ts
@@ -0,0 +1,10 @@
+export const phases = {
+  DRAWDECISION: "drawDecision",
+  DRAWPILEDECISION: "drawPileDecision",
+  DRAWPILEDISCARD: "drawPileDiscard",
+  DRAWPILEKEEP: "drawPileKeep",
+  DISCARDPILEDECISION: "discardPileDecision",
+  DISCARDPILEREPLACEOPEN: "discardPileReplaceOpen",
+  DISCARDPILEREPLACEHIDDEN: "discardPileReplaceHidden",
+  WAITTURN: "waitTurn",
+};
