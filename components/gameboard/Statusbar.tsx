566f274109b5f6ce5b7eeb1533459db645a402d1
fre-ben <fschocke@gmail.com>
2021-04-07 12:09:37 +0200

    ✨ Add statusbar dynamically displaying status messages Depending on what the player has to do, the statusbar shows a related message

--- components/gameboard/Statusbar.tsx
+++ components/gameboard/Statusbar.tsx
@@ -1,7 +1,30 @@
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../../contexts/SocketContext";
+import { getLobbyNr } from "../../lib/functions";
+import { status } from "../../server/lib/statusMessages";
 import styles from "./MiscElements.module.css";
-import type { StatusbarProps } from "./types";
+import { StatusbarProps } from "./types";
 
-function Statusbar({ statusMessage }: StatusbarProps) {
+function Statusbar({ activePlayer }: StatusbarProps) {
+  const { socket } = useContext(SocketContext);
+  const lobbyNr = getLobbyNr();
+  const [statusMessage, setStatusMessage] = useState<string>(status.PREREADY);
+
+  useEffect(() => {
+    if (!socket || !lobbyNr) {
+      return;
+    }
+
+    function handleDisplayStatus(statusMessage: string) {
+      setStatusMessage(statusMessage);
+    }
+
+    socket.on("display status", handleDisplayStatus);
+    return () => {
+      socket.off("display status", handleDisplayStatus);
+    };
+  }, [socket, lobbyNr, activePlayer]);
+
   return (
     <>
       <p className={styles.statusbar}>
