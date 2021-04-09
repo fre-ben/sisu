15229337a2c55f5117a6c667916f465e80a08d58
fre-ben <fschocke@gmail.com>
2021-04-09 16:45:24 +0200

    ✨ Add DrawPileProps

--- components/gameboard/types.ts
+++ components/gameboard/types.ts
@@ -1,15 +1,13 @@
 import { MouseEventHandler } from "react";
 import { ActivePlayer, Card } from "../../server/lib/gameTypes";
 
-export type PileProps = {
-  onClick: MouseEventHandler<HTMLButtonElement>;
-};
-
 export type DiscardPileProps = {
   onPileClick?: MouseEventHandler<HTMLImageElement>;
   card: Card;
   turnPhase: string;
 };
+
+export type DrawPileProps = Pick<DiscardPileProps, "turnPhase">;
 
 export type RoundCounterProps = {
   roundNr: number;
